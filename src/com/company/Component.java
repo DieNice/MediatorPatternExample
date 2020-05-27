a29ad48dc9bebf25c1103afe6eed15c31c1f9b25
DieNice <DailyPlanning@bk.ru>
2020-05-27 14:11:55 +1000

    added working example

---
+++ src/com/company/Component.java
@@ -0,0 +1,6 @@
+package com.company;
+//Общий интерфейс компонентов
+public interface Component {
+    void setMediator(Mediator mediator);
+    String getName();
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import com.company.components.*;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Mediator mediator = new Editor();
+
+        mediator.registerComponent(new Title());
+        mediator.registerComponent(new TextBox());
+        mediator.registerComponent(new AddButton());
+        mediator.registerComponent(new DeleteButton());
+        mediator.registerComponent(new SaveButton());
+        mediator.registerComponent(new List(new DefaultListModel()));
+        mediator.registerComponent(new Filter());
+
+        mediator.createGUI();
+    }
+}
